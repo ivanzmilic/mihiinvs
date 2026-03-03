7c62a65d3a69d89a75c4cff142ebf91e7862f4c4
Ivan Milic <ivanzmilic@gmail.com>
2026-03-03 15:19:34 +0100

    minor edits for Saida

--- assemble_stokes_4D.py
+++ assemble_stokes_4D.py
@@ -12,7 +12,8 @@ path = sys.argv[1]
 #path = '/dat/milic/Na_MiHI/plage/obs01/mihi_sp/lr0500/'
 
 start = 0 
-step = 1600
+step = 1600 # This is amount of frames in one cube.
+            # if you need time, typically there is 600 frames per second.
 
 last = 1537600
 #last = start + 5*step
@@ -36,9 +37,11 @@ for frame in tqdm(range(start, last, step)):
     '''
 
     # But for Cori, we want full resolution cubes, and we will only do Stokes I and V, also clean up wavelengths a bit.
+    
     fullpath = path+'img_fit.'+str(frame)+'..'+str(frame+step)+'.00.00.lr0500.cube.fits'
+    
     stokes = fits.open(fullpath)[0].data[12:-12,12:-12,[0,3],35:475]
-    ll = fits.open(fullpath)[1].data[0:500]
+    ll = fits.open(fullpath)[1].data[35:475]
     stokes = stokes.reshape(1, stokes.shape[0], stokes.shape[1], stokes.shape[2],-1)
     
     
@@ -55,6 +58,11 @@ print(cube.shape)
 print(llseries.shape)
 
 hdu = fits.PrimaryHDU(cube)
+# Add some header information here:
+hdu.header['XSTEP'] = 0.059 # step in arcseconds
+hdu.header['YSTEP'] = 0.059 # step in arcseconds
+hdu.header['TSTEP'] = step/600.0 # timestep in seconds, assuming 600 frames per second
+
 hdu1 = fits.ImageHDU(llseries)
 hdul = fits.HDUList([hdu, hdu1])
 hdul.writeto(path+'cubeseries_Na_cori.fits', overwrite=True)
